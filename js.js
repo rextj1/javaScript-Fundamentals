909206af776b4cb98103a740e34bc061e1898d6f
rextj1 <rextj121@gmail.com>
2025-11-03 22:29:47 +0100

    Implement local storage

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
-console.log('javaScript Fundamentals');
+// Store a value in local storage
+localStorage.setItem("username", "Toju");
+//Retrieve it
+let name = localStorage.getItem("username");
+console.log(name); // Toju
+// Remove one item
+localStorage.removeItem("username");
+// Clear all items
+localStorage.clear();
