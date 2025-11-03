8432d4237bd9a429fc20866b05c53b2615d49c6a
rextj1 <rextj121@gmail.com>
2025-11-03 11:59:29 +0100

    Implement hide or show a text element

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-console.log('javaScript Fundamentals');
+let text = document.getElementById("title");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+})
