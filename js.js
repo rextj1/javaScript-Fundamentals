06f341131248758ea0877797cda143dc6218b1b2
rextj1 <rextj121@gmail.com>
2025-11-03 15:39:10 +0100

    Implement to do list

--- js.js
+++ js.js
@@ -1,1 +1,22 @@
-console.log('javaScript Fundamentals');
+let taskInput = document.getElementById("taskInput");
+let addTaskBtn = document.getElementById("addTask");
+let taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", () => {
+    let task = taskInput.value.trim()
+
+    if (task === "") return;
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    // Remove task on click
+    li.addEventListener("click", () => {
+        li.remove();
+    });
+
+    taskList.appendChild(li);
+    
+    // Clear input field
+    taskInput.value = "";
+})
+
