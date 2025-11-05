0cbcf548fa1cb0a2b3782af49af6e868e69bec7f
rextj1 <rextj121@gmail.com>
2025-11-05 01:56:04 +0100

    Add persistent counter functionality

--- counter.js
+++ counter.js
@@ -1,21 +1,31 @@
-let count = 0;
+let count = localStorage.getItem('count') ? parseInt(localStorage.getItem('count')) : 0;
 
 let DesplayCount = document.getElementById('count');
 let decreaseCount = document.getElementById('decrease');
 let increaseCount = document.getElementById('increase');
 let resetCount = document.getElementById('reset');
 
+// Initial display or This ensures the user sees the correct 
+// starting number as soon as the page finishes loading.
+DesplayCount.textContent = count;
+
 increaseCount.addEventListener('click', () => {
     count++;
-    DesplayCount.textContent = count;
+    updateDisplay();
+
 });
 
 decreaseCount.addEventListener('click', () => {
     count--;
-    DesplayCount.textContent = count;
+    updateDisplay()
 });
 
 resetCount.addEventListener('click', () => {
     count = 0;
-    DesplayCount.textContent = count;
+    updateDisplay()
 })
+
+function updateDisplay() {
+    localStorage.setItem('count', count);
+    DesplayCount.textContent = count;
+}
