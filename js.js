0baa90e7d574c44cb0a0eb536f87b26e41f04d26
rextj1 <rextj121@gmail.com>
2025-12-04 21:47:04 +0100

    Implement JavaScript class

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
-console.log('javaScript Fundamentals');
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(this.name + " is eating");
+  }
+}
+
+class Dog extends Animal {
+   constructor(name, color) {
+    super(name); // call parent constructor
+    this.color = color;
+  }
+  bark() {
+    console.log(`Woof!, this ${this.name} is ${this.color}`);
+  }
+}
+
+let d = new Dog("Bingo", "brown");
+d.eat();  // inherited from Animal
+d.bark(); // from Dog
