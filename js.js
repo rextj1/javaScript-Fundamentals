08550dc82729f45d530f1fe53a3d4099c4767d5a
rextj1 <rextj121@gmail.com>
2025-12-09 11:15:35 +0100

    implement event model

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
-console.log('javaScript Fundamentals');
+// menu.addEventListener("click", (e) => {
+//   console.log("target:", e.target);
+//   console.log("current:", e.currentTarget);
+//     console.log("User selected:", e.target.textContent);
+// });
+
+// Event Delegation (Essential Skill)
+// Instead of adding listeners to every child element, attach one listener to the parent and check the target.
+
+// menu.addEventListener("click", (e) => {
+//   if (e.target.tagName === "LI") {
+//     console.log("User selected:", e.target.textContent);
+//   }
+// });
+
+
+
+
