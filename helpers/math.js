9f23cb994f8a85b01af906959bbb54b55b87d283
rextj1 <rextj121@gmail.com>
2025-12-09 12:39:04 +0100

    implement javaScript Modules (import/export

---
+++ helpers/math.js
@@ -0,0 +1,9 @@
+
+export const add = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+export const PI = 3.14;
+
+export default function greet(name) {
+  return `Hello ${name}`;
+}
+
--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-console.log('javaScript Fundamentals');
+import greet, {add, multiply, PI} from "./helpers/math.js";
+console.log(greet("Alice")); // Hello Alice
+console.log("Add:", add(2, 3)); // Add: 5
+console.log("Multiply:", multiply(4, 5)); // Multiply: 20
+console.log("PI:", PI); // PI: 3.14
+
+console.log('l');
+
