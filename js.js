2d6c8f80e8ef97ccaa29bac05d1b766a2e366e6f
rextj1 <rextj121@gmail.com>
2025-12-20 15:09:15 +0100

    implement javaScript closures

--- js.js
+++ js.js
@@ -1,1 +1,34 @@
-console.log('javaScript Fundamentals');
+function accountBalance(start) {
+    let balance = start;
+
+    return {
+        deposit: (amount) => { 
+            return balance + amount
+        },
+
+        withdrawal(amount) {
+            return balance - amount
+        }
+    }
+}
+
+let acctBal = accountBalance(100);
+
+console.log(acctBal.deposit(50));
+console.log(acctBal.withdrawal(30));
+
+// counter example
+function makeClickCounter(buttonId) {
+    let count = 0;
+    let button = document.createElement('button');
+    button.textContent = 'Click Me'
+    button.addEventListener("click", () => {
+        count++;
+        console.log("Clicked:", count);
+    });
+    document.body.append(button);
+}
+
+makeClickCounter("btn1");
+
+
