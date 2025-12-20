8f3abf4f8823dd8146d7093a1770ca66ffe179ad
rextj1 <rextj121@gmail.com>
2025-12-20 16:15:07 +0100

    add accountBalance class

--- js.js
+++ js.js
@@ -1,23 +1,46 @@
 class Animal {
-  constructor(name) {
-    this.name = name;
-  }
+    constructor(name) {
+        this.name = name;
+    }
 
-  eat() {
-    console.log(this.name + " is eating");
-  }
+    eat() {
+        console.log(this.name + " is eating");
+    }
 }
 
 class Dog extends Animal {
-   constructor(name, color) {
-    super(name); // call parent constructor
-    this.color = color;
-  }
-  bark() {
-    console.log(`Woof!, this ${this.name} is ${this.color}`);
-  }
+    constructor(name, color) {
+        super(name); // call parent constructor
+        this.color = color;
+    }
+    bark() {
+        console.log(`Woof!, this ${this.name} is ${this.color}`);
+    }
 }
 
 let d = new Dog("Bingo", "brown");
 d.eat();  // inherited from Animal
 d.bark(); // from Dog
+
+
+//  class
+class AccountBalance {
+    #balance;
+    constructor(start) {
+        this.#balance = start;
+    }
+
+    deposit(amount) {
+        this.#balance += amount;
+        return this.#balance;
+    }
+    withdrawal(amount) {
+        this.#balance -= amount;
+        return this.#balance;
+    }
+}
+
+let acctBal = new AccountBalance(100)
+console.log(acctBal.deposit(50));
+console.log(acctBal.deposit(50));
+console.log(acctBal.withdrawal(30));
